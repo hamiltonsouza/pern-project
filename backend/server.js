948f3e327f20f4220dd2778403485f6436b89e91
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-06 16:33:38 -0300

    Add helmet to root

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
+
 app.get("/", (req, res) => {
     res.send("test")
-})
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
