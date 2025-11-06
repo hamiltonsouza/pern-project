cbd8cb7a4c1baf756542d209ca4c5ab00156781b
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-06 16:40:12 -0300

    Add .env and declare PORT constant

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config;
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet()); // security middleware
 app.use(morgan("dev")); // requests logger
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
     res.send("test")
 });
 
-app.get("/test", (req, res) => {
+app.get("/", (req, res) => {
     res.send("test")
 });
 
