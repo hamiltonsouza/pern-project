fd30e7c5cb8469f75b36c9f38a468b6fa24c51e9
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-06 19:43:01 -0300

    Add delete put request

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -78,4 +78,24 @@ export const updateProduct = async (req, res) => {
     }
 };
 
-export const deleteProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {
+    const {id} = req.params
+
+    try {
+        const deletedProduct = await sql`
+            DELETE FROM products
+            WHERE id=${id}
+            RETURNING *
+        `;
+
+        if(deletedProduct.length === 0) {
+            return res.status(404).json({ success: false, message: "Product not found." });
+        }
+
+        res.status(200).json({ success: true, data: deletedProduct[0]});
+
+    } catch (error) {
+        console.log("Error deleting product.", error);
+        res.status(500).json({ success:false, message: "Internal Server Error."})
+    }    
+};
