d1d53a76cb3ee6067fe3f0c349c85fb3e73ae8a3
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-06 19:38:26 -0300

    Add update product put request

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -54,5 +54,28 @@ export const getProduct = async (req, res) => {
     }
 };
 
-export const updateProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {
+        const {id} = req.params
+        const {name, price, image} = req.body;
+
+    try {
+        const updateProduct = await sql`
+            UPDATE products
+            SET name=${name}, price=${price}, image=${image}
+            WHERE id=${id}
+            RETURNING *
+        `
+
+        if(updateProduct.length === 0) {
+            return res.status(404).json({ success: false, message: "Product not found." });
+        }
+
+        res.status(200).json({ success: true, data: updateProduct[0]});
+
+    } catch (error) {
+        console.log("Error updating product.", error);
+        res.status(500).json({ success:false, message: "Internal Server Error."})
+    }
+};
+
 export const deleteProduct = async (req, res) => {};
