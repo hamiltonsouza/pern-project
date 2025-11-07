ef45ad0f3d8d8e6a55f366b54ba89d59c709093f
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-06 23:38:44 -0300

    Add product delete request

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,6 +13,6 @@ router.get("/", getAllProducts);
 router.get("/:id", getProduct);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
-router.put("/:id", deleteProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
