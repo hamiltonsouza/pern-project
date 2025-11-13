c9c02719eb2408d50b60a3d80321e18c335e0e03
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-13 16:02:06 -0300

    Add no products message

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useProductStore } from '../store/useProductStore.js'
-import { PlusCircleIcon, RefreshCwIcon } from "lucide-react";
+import { PackageIcon, PlusCircleIcon, RefreshCwIcon } from "lucide-react";
 import ProductCard from '../components/ProductCard.jsx';
 
 function HomePage() {
@@ -13,7 +13,7 @@ function HomePage() {
   return (
     <main className="max-w-6xl mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8">
-        <button className="size-5 mr-2">
+        <button className="btn btn-primary">
           <PlusCircleIcon className="size-5 mr-2" />
           Add Product
         </button>
@@ -22,6 +22,21 @@ function HomePage() {
         </button>
       </div>
       {error && <div className="alert alert-error mb-8">{error} </div>}
+
+      {products.length === 0 && !loading && (
+        <div className="flex flex-col justify-center items-center h-96 space-y-4">
+          <div className="bg-base-100 rounded-full p-6">
+            <PackageIcon className="size-12" />
+          </div>
+          <div className="text-center space-y-2">
+            <h3 className="text-2xl font-semibold">No products found.</h3>
+            <p className="text-gray-500 max-w-sm">
+              Get started by adding your first product to the inventory.
+            </p>
+          </div>
+        </div>
+      )}
+
       {loading ? (
         <div className="flex justify-center items-center h-64">
           <div className="loading loading-spinner loading-lg" />
