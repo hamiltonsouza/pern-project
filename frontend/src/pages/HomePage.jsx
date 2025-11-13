efa253bcdbc040e3e52b0b7d0f6efc6310c9881b
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-13 15:00:38 -0300

    Add product fetching API integration

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useProductStore } from '../store/useProductStore.js'
 
 function HomePage() {
+  const {products, loading, error, fetchProducts} = useProductStore();
+
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
+
+  console.log("products",products);
+
   return (
     <div>HomePage</div>
   )
