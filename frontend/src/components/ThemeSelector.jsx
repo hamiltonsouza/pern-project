4cf5d56a5ccd2972c04b0418ef26cdcb352f22b6
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-12 22:49:39 -0300

    Add theme selector function

--- frontend/src/components/ThemeSelector.jsx
+++ frontend/src/components/ThemeSelector.jsx
@@ -1,23 +1,27 @@
 import { PaletteIcon } from "lucide-react";
 import { THEMES } from "../constants/index.js";
+import { useThemeStore } from "../store/useThemeStore.js";
 
 function ThemeSelector() {
-    const theme = "forest";
+    const { theme, setTheme } = useThemeStore;
     return (
         <div className="dropdown dropdown-end">
             <button tabIndex={0} className="btn btn-ghost btn-circle">
                 <PaletteIcon className="size-5" />
             </button>
             <div tabIndex={0}
-                className="dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl w-56 border border-base-content/10"> 
+                className="dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl
+                w-56 border border-base-content/10"
+            > 
                 {THEMES.map(themeOption => (
                     <button
-                    key={themeOption.name}
-                    className={`
-                        w-full px-4 py-3 rounded-xl flex items-center gap-3 transition-colors
-                        ${theme === themeOption.name ? "bg-primary/10 text-primary" : "hover:bg-base-content/5"}
-                        `} 
-                        >
+                        key={themeOption.name}
+                        className={`
+                            w-full px-4 py-3 rounded-xl flex items-center gap-3 transition-colors
+                            ${theme === themeOption.name ? "bg-primary/10 text-primary" : "hover:bg-base-content/5"}
+                            `}
+                        onClick={() => setTheme(themeOption.name)}
+                    >
                         <PaletteIcon className="size-4" />
                         <span className="text-sm font-medium">{themeOption.label}</span>
                         <div className="ml-auto flex gap-1">
