3103b109e00c42a949e925c5a56f170f136c64e3
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-14 15:41:19 -0300

    Add product modal form

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useProductStore } from '../store/useProductStore.js'
 import { PackageIcon, PlusCircleIcon, RefreshCwIcon } from "lucide-react";
 import ProductCard from '../components/ProductCard.jsx';
+import AddProductModal from '../components/AddProductModal.jsx';
 
 function HomePage() {
   const {products, loading, error, fetchProducts} = useProductStore();
@@ -13,7 +14,7 @@ function HomePage() {
   return (
     <main className="max-w-6xl mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8">
-        <button className="btn btn-primary">
+        <button className="btn btn-primary" onClick={() => document.getElementById("add_product_modal").showModal()}>
           <PlusCircleIcon className="size-5 mr-2" />
           Add Product
         </button>
@@ -21,6 +22,9 @@ function HomePage() {
           <RefreshCwIcon className="size-5" />
         </button>
       </div>
+
+      <AddProductModal />
+
       {error && <div className="alert alert-error mb-8">{error} </div>}
 
       {products.length === 0 && !loading && (
