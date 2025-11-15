4001fb2a55c805441148720a852a1c0ab497c06c
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2025-11-15 07:18:19 -0300

    Add loading and error handling and homepage button

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useProductStore } from "../store/useProductStore.js";
+import { useNavigate, useParams } from "react-router-dom";
+import { ArrowLeftIcon } from "lucide-react";
 
 function ProductPage() {
+
+  const {
+    currentProduct,
+    formData,
+    setFormData,
+    loading,
+    error,
+    fetchProduct,
+    updateProduct,
+    deleteProduct,
+  } = useProductStore();
+
+  const navigate = useNavigate();
+
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetchProduct(id);
+  }, [fetchProduct, id]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="loading loading-spinner loading-lg" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <div className="alert alert-error">{error}</div>
+      </div>
+    );
+  }
+  
   return (
-    <div>ProductPage</div>
-  )
+
+    <div className="container mx-auto px-4 py-8 max-w-4xl">
+      <button onClick={() => navigate("/")} className="btn btn-ghost mb-8">
+        <ArrowLeftIcon className="size-4 mr-2" />
+        Back to Products
+      </button>
+    </div>
+  );
 }
 
-export default ProductPage
+export default ProductPage;
